6c6c1f7ef2549c0acea14c80a09c61739e065c1f
Alexander Lehmann <a.lehm@gmx.de>
2015-06-08 12:50:07 +0200

    added ControlStation component (eclipse project)

---
+++ components/ControlStation/src/de/ees/group1/cs/gui/MainWindow.java
@@ -0,0 +1,48 @@
+package de.ees.group1.cs.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblHalloWelt = new JLabel("Hallo Welt!");
+		frame.getContentPane().add(lblHalloWelt, BorderLayout.NORTH);
+	}
+
+}
