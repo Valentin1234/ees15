a31d4286abd848d0cf0e697a79ce82dd0bf2b12b
Alexander Lehmann <a.lehm@gmx.de>
2015-06-11 11:14:10 +0200

    fehlende jars eingechecked

--- components/ControlStation/src/de/ees/group1/cs/gui/MainWindow.java
+++ components/ControlStation/src/de/ees/group1/cs/gui/MainWindow.java
@@ -1,32 +1,18 @@
 package de.ees.group1.cs.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
-import javax.swing.Box;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.border.BevelBorder;
-import java.awt.GridLayout;
-import javax.swing.BoxLayout;
-import javax.swing.border.TitledBorder;
-import java.awt.FlowLayout;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
-import java.awt.Component;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.factories.FormFactory;
-import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.border.TitledBorder;
 import net.miginfocom.swing.MigLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 
 public class MainWindow {
 
@@ -84,12 +70,13 @@ public class MainWindow {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "anstehende Auftr\u00E4ge", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_1.setLayout(new BorderLayout(0, 0));
+		panel.setLayout(new MigLayout("", "[50%][50%]", "[331px]"));
+		panel.setLayout(new MigLayout("", "[50%][50%]", "[100%]"));
+		panel.add(panel_1, "cell 0 0,grow");
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "aktueller Auftrag", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_2.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		panel.setLayout(new MigLayout("", "[50%][50%]", "[331px]"));
-		panel.add(panel_1, "cell 0 0,grow");
 		panel.add(panel_2, "cell 1 0,grow");
 	}
 }
