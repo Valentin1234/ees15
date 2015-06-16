c434b03d8e4bca2d286ae2e51a80a50e4a964a40
Alexander <a.lehm@gmx.de>
2015-06-16 20:30:40 +0200

    added interfaces and method stubs to ControlStation controller

--- components/ControlStation/src/de/ees/group1/cs/controller/ControlStation.java
+++ components/ControlStation/src/de/ees/group1/cs/controller/ControlStation.java
@@ -1,5 +1,27 @@
 package de.ees.group1.cs.controller;
 
-public class ControlStation {
+import de.ees.group1.com.IControlStation;
+import de.ees.group1.cs.gui.IGUIListener;
+import de.ees.group1.model.ProductionOrder;
+
+public class ControlStation implements IGUIListener, IControlStation {
+
+	@Override
+	public void orderCreatedAction(ProductionOrder order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void orderRemovedAction(int orderID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reachedParkingPositionInd(int orderID, int nextWorkingStep) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
