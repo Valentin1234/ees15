8fe93c7fec1971b827072a9caf4fe5c91a045f1f
Steich <s0660494@tu-dresden.de>
2015-07-01 16:25:02 +0200

    creating a method for getting the next step

--- components/ComInterface/src/de/ees/group1/cont/ControlStation.java
+++ components/ComInterface/src/de/ees/group1/cont/ControlStation.java
@@ -8,13 +8,20 @@ import de.ees.group1.model.*;
 
 public class ControlStation {
 	private ProductionOrder currentOrder;
-	
+	private ProductionStep currentStep;
+	private int currentStepNumber;
 	
 	/*
 	 * übergibt den gerade an den NXT übermittleten Auftrag an die ControlStation
 	 */
 	public void getCurrentOrder(ProductionOrder order){
 		currentOrder=order;
+		currentStepNumber=0;
 	}
+	
+	public void getCurrentStep(){
+		currentStep =currentOrder.getStep(currentStepNumber);
+	}
+	
 
 }
