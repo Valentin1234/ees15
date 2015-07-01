4fb2dcfa8d2d750f43fe2cef5257963215131e54
Steich <s0660494@tu-dresden.de>
2015-07-01 16:09:40 +0200

    Creating new method getCurrentOrder which introduces the current Order

--- components/ComInterface/src/de/ees/group1/cont/ControlStation.java
+++ components/ComInterface/src/de/ees/group1/cont/ControlStation.java
@@ -2,7 +2,19 @@ package de.ees.group1.cont;
 /*
  * Controller for ControlStation, which handles order tracking
  */
+
+import de.ees.group1.model.*;
+
+
 public class ControlStation {
+	private ProductionOrder currentOrder;
 	
+	
+	/*
+	 * übergibt den gerade an den NXT übermittleten Auftrag an die ControlStation
+	 */
+	public void getCurrentOrder(ProductionOrder order){
+		currentOrder=order;
+	}
 
 }
